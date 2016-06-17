d97c7e7ea65a2ffa2b65e6e74a6a4bae6a8f6e1d
Chris Flint <flintedc@gmail.com>
2016-06-17 10:53:52 +0100

    added guest creation  testing

--- specs/guest_spec.rb
+++ specs/guest_spec.rb
@@ -8,4 +8,12 @@ class TestGuest < MiniTest::Test
     @guest1 = Guest.new("Mike", "metal", 100, 1)
     @guest2 = Guest.new("Susan", "rock", 150, 0.5)
   end 
+
+  def test_guest_creation
+    assert_equal("Mike", @guest1.name)
+  end
+
+  def test_guest_creation_2
+    assert_equal("rock", @guest2.genre)
+  end
 end
